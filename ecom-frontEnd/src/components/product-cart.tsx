537dc1cc9f32fa749f45f8bcc883c0a8551a69f0
Akash1211222 <90515043+Akash1211222@users.noreply.github.com>
2023-12-30 02:49:42 +0530

    home componennt comp ui

---
+++ ecom-frontEnd/src/components/product-cart.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { FaPlus } from 'react-icons/fa';
+
+
+const server = "asdfghjkjnbvcxf"
+
+type ProductsProps = {
+    productId: string;
+    photo: string;
+    name: string;
+    price: number;
+    stock: number;
+    handler: () => void;
+}
+
+const ProductCart = ({productId, photo, name, price, stock, handler } :ProductsProps) => {
+  return (
+      <div className="product-card">
+          <img src={photo} alt={name} />
+          <p>{name}</p>
+          <span>₹{price}</span>
+          
+          <div>
+              <button onClick = {()=> handler()}>
+                  <FaPlus />
+              </button>
+          </div>
+    </div>
+  )
+}
+
+export default ProductCart;
--- ecom-frontEnd/src/pages/home.tsx
+++ ecom-frontEnd/src/pages/home.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import ProductCart from '../components/product-cart'
 
 const Home = () => {
+
+  const addToCartHandler = () => { };
+
   return (
-    <div>home</div>
+    <div className="home">
+      <section></section>
+
+      <h1>Latest Products
+        <Link to="/search" className='findmore'>
+          More
+        </Link>
+      </h1>
+      
+
+      <main>
+        <ProductCart
+          productId='akakak'
+          name='Apple'
+          price={90000}
+          stock={100}
+          handler={addToCartHandler}
+          photo="https://m.media-amazon.com/images/W/MEDIAX_792452-T2/images/I/71d7rfSl0wL._SX679_.jpg"
+        />
+      </main>
+   </div> 
   )
 }
 
